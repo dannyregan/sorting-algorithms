2d22c1ba7ba00f82aabe3d41b6f4035ae8cf915a
Danny Regan <d.regan@columbia.edu>
2024-11-11 10:52:57 +0100

    Bubble sort function compares neighboring elements and swaps them if they're not in the right order.

--- bubble-sort.py
+++ bubble-sort.py
@@ -1,10 +1,14 @@
-def bubbleSort(a):
-    return
+def bubbleSort(A):
+    for i in range(len(A) - 1):
+        for j in range(len(A) - i - 1):
+            if (A[j] > A[j + 1]):
+                A[j], A[j + 1] = A[j + 1], A[j]
+    return f"Final array = [{A[0]}], [{A[1]}], [{A[2]}]...[{A[-3]}], [{A[-2]}], [{A[-1]}]"
 
 # =======================
 A4 = [44, 63, 77, 17, 20, 99, 84, 6, 39, 52]
 A5 = [52, 84, 6, 39, 20, 77, 17, 99, 44, 63]
 A6 = [6, 17, 20, 39, 44, 52, 63, 77, 84, 99]
-bubbleSort(A4)
-bubbleSort(A5)
-bubbleSort(A6)
+print(bubbleSort(A4))
+print(bubbleSort(A5))
+print(bubbleSort(A6))
