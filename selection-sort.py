f2bb2f31da7782adad9a74b020136a94d1fec9d3
Danny Regan <d.regan@columbia.edu>
2024-11-11 10:18:48 +0100

    Selection sort brings smallest number forward each iteration.

--- selection-sort.py
+++ selection-sort.py
@@ -1,10 +1,23 @@
-def selectionSort(a):
-    return
+def selectionSort(A):
+    compareCount = 0
+    swapCount = 0
+    for i in range(len(A)-1):
+        print(f"Elements have ben compared {compareCount} times.")
+        print(f"Elements have ben swapped {swapCount} times.")
+        print(f"Current status of the array: {A}")
+        minIndex = i
+        for j in range(i + 1, len(A)):
+            compareCount += 1
+            if A[j] < A[minIndex]:
+                swapCount += 1
+                minIndex = j
+        A[i], A[minIndex] = A[minIndex], A[i]
+    return f"A = [{A[0]}], [{A[1]}], [{A[2]}]...[{A[-3]}], [{A[-2]}], [{A[-1]}]"
 
 # =======================
 A1 = [63, 44, 17, 77, 20, 6, 99, 84, 52, 39]
 A2 = [84, 52, 39, 6, 20, 17, 77, 99, 63, 44]
 A3 = [99, 84, 77, 63, 52, 44, 39, 20, 17, 6]
-selectionSort(A1)
-selectionSort(A2)
-selectionSort(A3)
+print(selectionSort(A1))
+print(selectionSort(A2))
+print(selectionSort(A3))
